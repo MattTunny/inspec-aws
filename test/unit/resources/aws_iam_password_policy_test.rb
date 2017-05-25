8ca8097fbe9df7a50e5600f07aee918b4d2da9db
sfreeman <Steffanie.Freeman@d2l.com>
2017-05-25 15:48:22 -0400

    Run linter on unit tests
    Signed-off-by: sfreeman <Steffanie.Freeman@d2l.com>

--- test/unit/resources/aws_iam_password_policy_test.rb
+++ test/unit/resources/aws_iam_password_policy_test.rb
@@ -5,34 +5,34 @@ require 'json'
 
 class AwsIamPasswordPolicyTest < Minitest::Test
   def setup
-    @mockConn = Minitest::Mock.new
-    @mockResource = Minitest::Mock.new
+    @mock_conn = Minitest::Mock.new
+    @mock_resource = Minitest::Mock.new
 
-    @mockConn.expect :iam_resource, @mockResource
+    @mock_conn.expect :iam_resource, @mock_resource
   end
 
   def test_policy_exists_when_policy_exists
-    @mockResource.expect :account_password_policy, true
+    @mock_resource.expect :account_password_policy, true
 
-    assert_equal true, AwsIamPasswordPolicy.new(@mockConn).exists?
+    assert_equal true, AwsIamPasswordPolicy.new(@mock_conn).exists?
   end
 
   def test_policy_does_not_exists_when_no_policy
-    @mockResource.expect :account_password_policy, nil do |args|
+    @mock_resource.expect :account_password_policy, nil do
       raise Aws::IAM::Errors::NoSuchEntity.new nil, nil
     end
 
-    assert_equal false, AwsIamPasswordPolicy.new(@mockConn).exists?
+    assert_equal false, AwsIamPasswordPolicy.new(@mock_conn).exists?
   end
 
   def test_throws_when_password_age_0
-    policyObject = Minitest::Mock.new
-    policyObject.expect :expire_passwords, false
+    policy_object = Minitest::Mock.new
+    policy_object.expect :expire_passwords, false
 
-    @mockResource.expect :account_password_policy, policyObject
+    @mock_resource.expect :account_password_policy, policy_object
 
     e = assert_raises Exception do
-      AwsIamPasswordPolicy.new(@mockConn).max_password_age
+      AwsIamPasswordPolicy.new(@mock_conn).max_password_age
     end
 
     assert_equal e.message, 'this policy does not expire passwords'
